da8ddfa958487db38aeacb44ab53018e4331956d
xrsQugi <02.04.5.art@gmail.com>
2023-05-01 12:23:27 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import * as goitHw from "./js/goit.js";
+
+goitHw.searchForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    goitHw.fetchImages(`https://pixabay.com/api/?key=35594812-0318ae570b601c4a3427f19fb&q=${goitHw.searchInput.value.toLowerCase()}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${goitHw.per_page}`)
+    .then(images => {
+        goitHw.page += 1;
+        goitHw.gallery.replaceChildren([]);
+        goitHw.renderImages(images["hits"]);
+        goitHw.renderHits(images["totalHits"]);
+    })  
+})
+
+goitHw.loadMore.addEventListener("click", (event)=>{
+    event.preventDefault();
+    goitHw.fetchImages(`https://pixabay.com/api/?key=35594812-0318ae570b601c4a3427f19fb&q=${goitHw.searchInput.value.toLowerCase()}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${goitHw.per_page}&page=${goitHw.page}`)
+    .then(images => {
+        goitHw.addImages(images["hits"]);
+        goitHw.calculateImages(images["totalHits"]);
+    })  
+})
---
+++ src/js/goit.js
@@ -0,0 +1,99 @@
+import Notiflix from 'notiflix';
+
+export const gallery = document.querySelector(".gallery");
+export const searchInput = document.querySelector(".search-input");
+export const searchForm = document.querySelector(".search-form");
+export const loadMore = document.querySelector(".load-more");
+
+export async function fetchImages (api) {
+    try{
+        const images = await fetch(api);
+        const response = await images.json();
+        return response;
+    } catch (error) {
+        return Notiflix.Notify.failure("error with API");
+    }
+}
+
+export let page = 1;
+export let per_page = 5;
+
+export function renderImages(images) {
+    if(images == ""){
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    }
+    else{
+        page += 1;
+        loadMore.style.display = 'block';
+
+        images.forEach(image => {
+            gallery.insertAdjacentHTML("beforeend", 
+            `<div class="photo-card">
+                <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+                <div class="info">
+                    <div class="info-item">
+                        <p class='title' >Likes</p>
+                        <p>${image.likes}</p>
+                    </div>
+                    <div class="info-item">
+                        <p class='title'>Views</p>
+                        <p>${image.views}</p>
+                    </div>
+                    <div class="info-item">
+                        <p class='title'>Comments</p>
+                        <p>${image.comments}</p>
+                    </div>
+                    <div class="info-item">
+                        <p class='title'>Downloads</p>
+                        <p>${image.downloads}</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        })
+    }
+}
+
+export function addImages(images) {
+    page += 1;
+
+    images.forEach(image => {
+        gallery.insertAdjacentHTML("beforeend", 
+        `<div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+                <div class="info-item">
+                    <p class='title' >Likes</p>
+                    <p>${image.likes}</p>
+                </div>
+                <div class="info-item">
+                    <p class='title'>Views</p>
+                    <p>${image.views}</p>
+                </div>
+                <div class="info-item">
+                    <p class='title'>Comments</p>
+                    <p>${image.comments}</p>
+                </div>
+                <div class="info-item">
+                    <p class='title'>Downloads</p>
+                    <p>${image.downloads}</p>
+                </div>
+            </div>
+        </div>
+        `)
+    })
+}
+
+export function calculateImages(images) {
+    const total = Math.ceil(images/per_page) + 1;
+    if(total < page){
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+}
+
+export function renderHits(images){
+    if(images === 0){
+        return;
+    }
+    Notiflix.Notify.info(`"Hooray! We found "${images} images.`);
+}
